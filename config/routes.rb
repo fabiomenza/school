28927d951a90d0eb363c17e51bc8cc6a63f9cc5d
student <student@studentvm.(none)>
2014-06-14 14:09:46 +0200

    Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,63 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+	
+	# Landmarks
+	get 'curricula' => 'cirricula#view'			# Tutti i curriculum
+	get 'courses' => 'courses#view'				# Tutti i corsi
+	get 'services' => 'services#view'			# Tutti i servizi
+	get 'contacts' => 'contacts#view'			# Contatti
+	get 'mission' => 'mission#view'				# Mission & Principles
+	get 'how_to_get_there' => 'how_to_get_there#view'	# Come arrivare
+	get 'structures' => 'structures#view'			# Tutte le strutture
+	get 'events' => 'events#view'				# Tutti gli eventi
+	get 'awards' => 'awards#view'				# Tutti i riconoscimenti
+	get 'bureaucracy' => 'bureaucracy#view'			# Burocrazia
+	get 'regulation' => 'regulation#view'			# Regolamento
+	get 'costs' => 'costs#view'				# Costi
+	get 'bursary' => 'bursary#view'				# Borse di studio
+	get 'history' => 'history#view'				# Storia della scuola
+	get 'alumni' => 'alumni#view'				# Alumni
+	
+	# Course pages
+	get 'course/:id' => 'course#view'
+	get 'course/:id/syllabus' => 'course#syllabus'
+	get 'course/:id/timetable' => 'course#timetable'
+	
+	# Teacher pages
+	get 'teacher/:id' => 'teacher#view'
+	get 'teacher/:id/curriculum' => 'teacher#curriculum'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+	# Curricula pages
+	get 'curriculum/:id' => 'curriculum#view'
+	get 'curriculum/:id/work_opportunity' => 'curriculum#work_opportunity'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+	# News pages
+	get 'news/:id' => 'news#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+	# Materials pages
+	get 'materials/:id' => 'materials#view'
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+	# Exams pages
+	get 'exam/:id' => 'exam#view'
+	
+	# Classrooms pages
+	get 'classroom/:id' => 'classroom#view'
+	get 'classroom/:id/how_to_get_there' => 'classroom#how_to_get_there'
+	get 'classroom/:id/availability' => 'classroom#availability'
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+	# Student's services pages
+	get 'service/:id' => 'service#view'
+	get 'service/:id/reference' => 'service#reference'
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+	# Structures pages
+	get 'structure/:id' => 'structure#view'
+	get 'structure/:id/map' => 'structure#map'
+	get 'structure/:id/photogallery' => 'structure#photogallery'
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+	# Events pages
+	get 'event/:id' => 'event#view'
+	get 'event/:id/get_involved' => 'event#get_involved'
+	
+	# School's awards pages
+	get 'awards/:id' => 'awards#view'
+	
 end
