0565ff4d605e7ef94e1d9ed57b8d46105b871177
student <student@studentvm.(none)>
2014-06-14 17:12:57 +0200

    Added first course

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -1,5 +1,9 @@
 class CourseController < ApplicationController
   def view
+	c = Course.find(params[:id])
+	@title = @name = c.name
+	@description = c.description
+	@program = c.program
   end
 
   def syllabus
