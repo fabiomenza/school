00cb1ea17763098316ee32c068b47fdcddff1c65
Jacopo Ferrigno <dipusone@yahoo.it>
2014-06-16 22:19:27 +0200

    Modificato il layout base della pagina!

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,9 @@
 class Course < ActiveRecord::Base
-  belongs_to :teacher, inverse_of:  :classroom
+  belongs_to :teacher, inverse_of:  :course
   has_and_belongs_to_many  :curriculum 
-  has_many :material, inverse_of:  :classroom
-  has_many :exam, inverse_of:  :classroom
-  has_one :classroom, inverse_of:  :classroom
-  has_many :news, inverse_of:  :classroom
+  has_many :material, inverse_of:  :course
+  has_many :exam, inverse_of:  :course
+  has_one :classroom, inverse_of:  :course
+  has_many :news, inverse_of:  :course
 
 end
