22b3ba4f64aebcada2cdec5ef0dbb877f12b0192
Jacopo Ferrigno <dipusone@yahoo.it>
2014-06-19 17:49:19 +0200

    Rewrited the course page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,7 @@
 class CoursesController < ApplicationController
   def view
 	@name = @title = "All courses"
-	@courses=Course.all
+	@courses=Course.order('name ASC').all.paginate(page: params[:page])
 	@courses_year=Course.order('accademic_year DESC').select('accademic_year').map{|item| item.accademic_year.year}.uniq
 	@curriculum=Curriculum.order('name ASC').select('id, name')
 
